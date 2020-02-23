09c466cfa9d40dc33f3c24f05aff74cfd6a53a14
LABZHG <hg250250@126.com>
2020-02-23 23:58:37 +0800

    Update yixingfcw_Req.py

--- yixingfcw_Req.py
+++ yixingfcw_Req.py
@@ -7,7 +7,7 @@ Created on Wed Feb 12 20:42:56 2020
 
 import requests
 from bs4 import BeautifulSoup
-import numpy as np
+import xlsxwriter
 
 
 def getData(htmlfile):
@@ -29,6 +29,13 @@ def getData(htmlfile):
     DataList = list(filter(None, finalList)) 
     return DataList
 
+def demoOutput(demoList):
+    Exfile=xlsxwriter.Workbook('demo.xlsx')
+    Exsheet=Exfile.add_worksheet(u'sheet1')
+    for i in range(len(demoList)):
+        Exsheet.write('A%s'%str(i+1),demoList[i])
+    Exfile.close()
+    
 def coreDataBeneficiate():
     pass
 
