9afe641ee8494fef4634b4d47c799a16f6aa890a
LABZHG <hg250250@126.com>
2020-03-02 23:29:31 +0800

    Update DataBase_mongo.py

--- DataBase_mongo.py
+++ DataBase_mongo.py
@@ -8,5 +8,15 @@ Created on Fri Feb 28 22:42:43 2020
 from pymongo import MongoClient
 
 Mongo_URL="mongodb://localhost:2100"
+dbName="FullMessage"
+dbTable="__date__"
 
 client=MongoClient(Mongo_URL)
+DBase=client[dbName]
+
+def insertData(data):
+    if (DBase[dbTable].insert(data)):
+        return True
+    else:
+        return False;
+    
