64b2ac7024d81a45cc063513c125bebd3ecec8d3
LABZHG <hg250250@126.com>
2020-03-05 23:58:59 +0800

    Update UIengine.py

--- bin/UIengine.py
+++ bin/UIengine.py
@@ -8,3 +8,15 @@ Created on Fri Feb 28 23:01:04 2020
 from flask import Flask, render_template
 
 App_engine=Flask(__name__)
+
+@App_engine.route('/')
+
+
+def UIhomepage():
+    home='FalskHome.html'
+    return render_template(home)
+
+
+
+if __name__ =="__main__":
+    App_engine.run(host='127.0.0.1',port=8342)
