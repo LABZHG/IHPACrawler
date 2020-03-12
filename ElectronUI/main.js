996dd16a0395701538f9f2e1fd1c06d705f22e7c
LABZHG <hg250250@126.com>
2020-03-12 22:59:58 +0800

    reinitialize

--- ElectronUI/main.js
+++ ElectronUI/main.js
@@ -1,21 +1,30 @@
 const electron = require('electron');
-const { appEngine } = electron;
-const { BrowserWindow } = electron;
+const appEngine = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+//console.log(electron);
 let win;
 function createWindow() {
-  win = new BrowserWindow({width: 800, height: 600});
-  win.loadURL(`file://${__dirname}/index.html`);
+  win = new BrowserWindow({width: 800, height: 600, webPreferences: {nodeIntegration: false}});
+  win.loadURL(`file://${__dirname}/index.html`)
 
   win.webContents.openDevTools();
-  win.on('closed', () => {
+  win.on('closed', function(){
     win = null;
   });
 }
 
 
-appEngine.on('ready', createWindow)
-app.on('window-all-closed', () => {
+appEngine.on('ready', createWindow);
+
+appEngine.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         appEngine.quit()
       }
-})
+});
+
+appEngine.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
