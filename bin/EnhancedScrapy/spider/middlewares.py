041504c0549b187d820d5323b5b50ec202a7151e
LABZHG <hg250250@126.com>
2020-03-31 22:42:30 +0800

    Update middlewares.py

--- bin/EnhancedScrapy/spider/middlewares.py
+++ bin/EnhancedScrapy/spider/middlewares.py
@@ -8,7 +8,7 @@
 from scrapy import signals
 
 
-class HellospiderSpiderMiddleware(object):
+class EnhancedspiderSpiderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
     # passed objects.
@@ -56,7 +56,7 @@ class HellospiderSpiderMiddleware(object):
         spider.logger.info('Spider opened: %s' % spider.name)
 
 
-class HellospiderDownloaderMiddleware(object):
+class EnhancedspiderDownloaderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
