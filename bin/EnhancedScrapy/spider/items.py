dcfbe299389a3b01ede7edfd1337c100f38637b4
LABZHG <hg250250@126.com>
2020-03-31 22:42:24 +0800

    Update items.py

--- bin/EnhancedScrapy/spider/items.py
+++ bin/EnhancedScrapy/spider/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class HellospiderItem(scrapy.Item):
+class EnhancedpiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
