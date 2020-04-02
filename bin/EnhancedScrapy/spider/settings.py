f20a6c66dd31ed2d09992bae058f6de1ac34f99d
LABZHG <hg250250@126.com>
2020-04-02 23:20:50 +0800

    Update settings.py

--- bin/EnhancedScrapy/spider/settings.py
+++ bin/EnhancedScrapy/spider/settings.py
@@ -9,10 +9,10 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'hellospider'
+BOT_NAME = 'Enhancedspider'
 
-SPIDER_MODULES = ['hellospider.spiders']
-NEWSPIDER_MODULE = 'hellospider.spiders'
+SPIDER_MODULES = ['Enhancedspider.spiders']
+NEWSPIDER_MODULE = 'Enhancedspider.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
@@ -22,7 +22,7 @@ NEWSPIDER_MODULE = 'hellospider.spiders'
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
